1d147a9f979540033353471bdb4f812491ab0065
andrey230502 <91019817+andrey230502@users.noreply.github.com>
2021-09-23 20:41:48 +0300

    Add files via upload

---
+++ Лаба№2  ООП/Задание 4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace задание_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("The current date and time is" + System.DateTime.Now);
+            Console.WriteLine("Input x + |r");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double J = Math.Exp(4 * x / 5) + Math.Sin(7 * Math.Log(x + 5 / 3)) + x / 5;
+            Console.WriteLine("x+{0}\t J = {1}", x, J);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Лаба№2  ООП/Задание №3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Лаба___2задание_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // вивести версіо о. с.
+            OperatingSystem os = System.Environment.OSVersion;
+            Console.WriteLine("Platform: {0}", os.Platform);
+            System.Console.WriteLine("The current date and time is" + System.DateTime.Now);  // Дата і час
+            System.Console.ReadLine();
+
+        }
+    }
+}
