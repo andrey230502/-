91aab010513b9a0de903e55f9936ad73f2f03067
andrey230502 <91019817+andrey230502@users.noreply.github.com>
2021-09-27 19:26:07 +0300

    Update Задание 4.cs

--- Лаба№2  ООП/Задание 4.cs
+++ Лаба№2  ООП/Задание 4.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace задание_4
 {
@@ -9,7 +9,7 @@ namespace задание_4
             System.Console.WriteLine("The current date and time is" + System.DateTime.Now);
             Console.WriteLine("Input x + |r");
             double x = Convert.ToDouble(Console.ReadLine());
-            double J = Math.Exp(4 * x / 5) + Math.Sin(7 * Math.Log(x + 5 / 3)) + x / 5;
+            double J = Math.Exp(4d * x / 5d) + Math.Sin(7d * Math.Log(x + 5d / 3d)) + x / 5d;
             Console.WriteLine("x+{0}\t J = {1}", x, J);
             Console.ReadKey();
         }
