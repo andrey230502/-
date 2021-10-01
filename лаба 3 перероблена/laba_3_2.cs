9c7acf8cac8936ca342b967396e9448240965b0e
andrey230502 <91019817+andrey230502@users.noreply.github.com>
2021-10-01 18:49:12 +0300

    Add files via upload

---
+++ лаба 3 перероблена/laba_3_2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace laba3_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode; // Розпізнавання укрїнських літер
+            string s;
+            double x, y;
+            StreamWriter f = new StreamWriter("laba3_2_.res"); // виведення 
+            StreamReader f1 = new StreamReader("laba3_2_.txt"); // введення
+            f.WriteLine("Таблиця \n********************************\nI      *X      *     Y     *\n********************************");
+        metka: s = f1.ReadLine();
+            if (s == null) goto metka1;
+            x = Convert.ToDouble(s);
+            y = 2d * Math.PI / Math.PI + Math.Pow(x, 2);
+            f.WriteLine("* x = {0:f3} * y = {1:e3} * \n", x, y);
+            goto metka;
+        metka1: f.WriteLine("*********************************\nРозробив Каранський Андрій Вадимович {0} \n", s);
+            f.Close();
+            f1.Close();
+        }
+    }
+}
---
+++ лаба 3 перероблена/Лаба3(1).cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace лаба3_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;// = -2.1;
+            int i;// = 444;
+            int c;// = 1000;
+            bool l;// = true;
+            string name;// = "Karanskiy";
+            Console.WriteLine("input a");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("input i");
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input c");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input l");
+            l = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("input фамілія");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Результати форматування \nName={0,1}\tl={1,4}", name, l);
+            System.Console.WriteLine("\na={0,1:f1}\ti={1,3}\tc={2,1}", a, i, c);
+            System.Console.WriteLine("\nЩоб вийти натисніть Enter");
+            Console.ReadLine();
+            System.Console.ReadLine();
+        }
+    }
+}
