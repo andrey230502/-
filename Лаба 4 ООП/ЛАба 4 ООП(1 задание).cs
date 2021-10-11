2d69e374e2e652dd19b6000056a595badf6a35fe
andrey230502 <91019817+andrey230502@users.noreply.github.com>
2021-10-11 15:23:00 +0300

    Add files via upload

---
+++ Лаба 4 ООП/ЛАба 4 ООП(1 задание).cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace laba_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double f;
+            double min;
+            double max;
+        m1: Console.WriteLine("Значення X");
+            double x = double.Parse((Console.ReadLine()));
+            Console.WriteLine("Значення Y");
+            double y = double.Parse((Console.ReadLine()));
+            Console.WriteLine("Значення Z");
+            double z = double.Parse((Console.ReadLine()));
+            if (x > y)
+            {
+                max = x;
+            }
+            else
+            {
+                max = y;
+            }
+            if (x + y > z)
+            {
+                min = x;
+            }
+            else
+            {
+                min = (x + y);
+            }
+            double c = max + Math.Abs(x + z);
+            if (c == 0)
+            {
+                Console.WriteLine("Знаменик дорівнює 0,щоб вести значення по новому натисніть будь-яку клавішу" + '\n' + "По закінченню програми натисніть Enter.");
+                string j = Console.ReadLine();
+                if (j != "") goto m1;
+                goto m1;
+            }
+            else
+            {
+                f = min / max + Math.Abs(x + z);
+            }
+            Console.WriteLine('\n' + "Дано:X=" + x + "Y=" + y + "Z=" + z + '\n' + ")Результат:F=" + f);
+            Console.WriteLine('\n' + "ДЛя повторного ввода" + "набрати будь-яку клавішу." + '\n' + "Для завершення програминатисніть Enter.");
+            string p = Console.ReadLine();
+            if (p != "") goto m1;
+
+
+
+
+            }
+    }
+}
---
+++ Лаба 4 ООП/Лаба 4 задание 2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace laba4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+        m1: Console.WriteLine("Точка M по " + "X=");
+            float x = float.Parse((Console.ReadLine()));
+            Console.WriteLine("Точка M по " + "Y=");
+            float y = float.Parse((Console.ReadLine()));
+            if (y > 0)
+            {
+                if (Math.Pow(x, 3) / y > 3) 
+                {
+                    if (x >= 0) N = 2;
+                    else N = 1;
+                }
+                else
+                {
+                    if (x >= 0) N = 4;
+                    else N = 2;
+                }
+            }
+            else
+            {
+                if (x >= 0) N = 4;
+                else N = 3;
+            }
+            Console.WriteLine('\t' + "Виходить:");
+            Console.WriteLine("M(" + x + ";" + y + ")" + " В області N=" + N);
+            Console.WriteLine('\n' + "Повторний ввод" + "Нажати любу клавішу" + '\n' + "Для завершення Enter");
+            string p = Console.ReadLine();
+            if (p != "") goto m1;
+        }
+    }
+}
