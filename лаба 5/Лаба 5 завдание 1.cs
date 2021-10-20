41aa48cc0d660903df2ebc82ba9559e152868870
andrey230502 <91019817+andrey230502@users.noreply.github.com>
2021-10-20 20:17:51 +0300

    Add files via upload

---
+++ лаба 5/Лаба 5 завдание 1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace laba_5__1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter p = new StreamWriter("laba5.txt");
+            float a, x, y;
+            p.WriteLine("Результат");
+            for (a = 0.5f; a <= 2.0f; a += 0.5f)
+            {
+                p.WriteLine("a=" + a);
+                for (x = -2; x <= 2; x += 0.05f) 
+                {
+                    if (a < 0)
+                    {
+                        p.WriteLine("Негативний");
+                    }
+                    if (Math.Sqrt(a) == 0)
+                    {
+                        p.WriteLine("На нуль ділити не можна");
+                    }
+                    else
+                    {
+                        y = (float)(Math.Pow(a, 3)) / (Math.Pow(a, 2)) + (Math.Pow(x, 2);
+                        p.WriteLine("   x=   " + x + '\t' + "   y=   " + y);
+                    }
+                }
+            }
+            p.Close();
+        }
+    }
+}
---
+++ лаба 5/лаба 5 задание 2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Лаба_5__2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, S, a, c, v, b, n;
+            int k;
+        m1: Console.WriteLine("Ввести значення x");
+            x = double.Parse((Console.ReadLine()));
+            Console.WriteLine('\t' + "Промежуточные результаты" + '\n');
+            k = 0;
+            S = 1; c = 1;
+            v = 0;
+            a = x;
+            while (Math.Abs(a) >= 0.000001)
+            {
+                S += a;
+                b = x * c;
+                n = b;
+                c += 2;
+                v = x * x;
+                a = 2 * (b / n);
+                k++;
+                Console.WriteLine('\t' + "Інтерація №" + k + '\n' + "a=" + a + "S=" + S + "\n");
+                y = x*Math.Cos(2*x);
+                Console.WriteLine('\t' + "   Результати:" + '\n' + "Заданий x=" + x + '\n' + "Сумма ряду S=" + S + '\n' + "Кількість членів ряду" + k + '\n' + "Функція x=" + y + '\n');
+                Console.WriteLine('\n' + "Для повторго ввода" + "Натиснути любу клавішу" + '\n' + "Для завершення дії натиснути Enter");
+                string rep = Console.ReadLine();
+                if (rep != "") goto m1;
+            }
+        }
+    }
+}
