1c3a3722d44ddc1cca84548a839dd9c6ddd764d5
andrey230502 <91019817+andrey230502@users.noreply.github.com>
2021-11-10 18:35:30 +0200

    Add files via upload

---
+++ ЛАБА 6 ооп.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ЛАБ_раб_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+            string strValue;
+            int[] iArray1 = new int[20];
+
+            for (j = 0; j < 20; j++)
+            {
+                strValue = Console.ReadLine();
+                iArray1[j] = Convert.ToInt32(strValue);
+
+            }
+            Console.WriteLine("-------------------Положительные индексы в масиве--------------------");
+            for (j = 0; j < 20; j++)
+            {
+                if (iArray1[j] >= 0)
+                {
+                    Console.WriteLine("iArray[" + j + "] =" + iArray1[j]);
+                }
+            }
+            Console.WriteLine("---------------------------------------------------------------------");
+        }
+    }
+}
