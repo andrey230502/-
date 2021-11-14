897e025d51bed7cbb058ccd821397188e73b0eaa
andrey230502 <91019817+andrey230502@users.noreply.github.com>
2021-11-14 22:19:53 +0200

    Add files via upload

---
+++ лаба7 ооп.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace лаба7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 4;
+
+            int i = 0, j = 0, k = N - 1;
+            int tmp;
+
+            int[,] array = new int[N, N]
+            {
+                {1, 0, 0, 7},
+                {0, 2, 6, 0},
+                {0, 5, 3, 0},
+                {4, 0, 0, 4},
+            };
+
+            for (int z = 0; z < N; z++)
+            {
+                for (int l = 0; l < N; l++)
+                {
+                    Console.Write("{0} ", array[z, l]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            while (i < 4)
+            {
+                tmp = array[i, j];
+                array[i, j] = array[i, k];
+                array[i, k] = tmp;
+                i++; j++; k--;
+            }
+
+            for (int z = 0; z < N; z++)
+            {
+                for (int l = 0; l < N; l++)
+                {
+                    Console.Write("{0} ", array[z, l]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
