c83856a72500912553e6a47daccbe19757bd1b62
andrey230502 <91019817+andrey230502@users.noreply.github.com>
2021-12-27 00:24:16 +0200

    Add files via upload

---
+++ лфба10.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            State s1 = new State("Тип правління");
+            Console.WriteLine("Клас State");
+            s1.Print();
+            s1.Clear();
+            s1.GetYear();
+            s1.MaxYear();
+            Console.WriteLine();
+            Republic r1 = new Republic("Республіка", "Народ");
+            Console.WriteLine("Клас Republic");
+            
+            r1.Print();
+            r1.ControlPeople(false);
+            r1.Clear();
+            Console.WriteLine();
+            Monarchy m1 = new Monarchy("Монархія", "Монарх");
+            Console.WriteLine("Клас Monarchy");
+            m1.Print();
+            Console.WriteLine();
+            Kingdom k1 = new Kingdom("Монархія", "Монарх", 1);
+            Console.WriteLine("Клас Kingdom");
+            k1.Grow();
+            k1.Falls();
+            k1.Print();
+            k1.Clear();
+            Console.WriteLine();
+        }
+    }
+}
